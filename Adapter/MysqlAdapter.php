dfffee88694b600abe3221aab48595bc51eb667d
clvarley <conorlukevarley@gmail.com>
2021-01-18 20:57:06 +0000

    Stubbed out the Mysql & PostgreSQL adapters

---
+++ Adapter/MysqlAdapter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Swiftly\Database\Adapter;
+
+use Swiftly\Database\AdapterInterface;
+
+/**
+ * Adapter used to work with MySQL databases
+ *
+ * @author clvarley
+ */
+Class MysqlAdapter Implements AdapterInterface
+{
+
+}
---
+++ Adapter/PostgresAdapter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Swiftly\Database\Adapter;
+
+use Swiftly\Database\AdapterInterface;
+
+/**
+ * Adapter used to work with PostgreSQL databases
+ *
+ * @author clvarley
+ */
+Class PostgresAdapter Implements AdapterInterface
+{
+
+}
