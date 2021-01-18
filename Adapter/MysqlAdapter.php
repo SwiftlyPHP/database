49ae996ba33c01e40e018d849d4f2dcf823be39b
clvarley <conorlukevarley@gmail.com>
2021-01-18 21:15:38 +0000

    Started fleshing out MySQL adapter

--- Adapter/MysqlAdapter.php
+++ Adapter/MysqlAdapter.php
@@ -2,7 +2,12 @@
 
 namespace Swiftly\Database\Adapter;
 
-use Swiftly\Database\AdapterInterface;
+use Swiftly\Database\{
+    AdapterInterface,
+    Connection
+};
+
+use mysqli;
 
 /**
  * Adapter used to work with MySQL databases
@@ -12,4 +17,94 @@ use Swiftly\Database\AdapterInterface;
 Class MysqlAdapter Implements AdapterInterface
 {
 
+    /**
+     * Database connection parameters
+     *
+     * @var Connection $connection Connection info
+     */
+    protected $connection;
+
+    /**
+     * Raw MySqli database handle
+     *
+     * @var mysqli $handle Database handle
+     */
+    protected $handle;
+
+    /**
+     * Creates an adapter using the given MySQL database info
+     *
+     * @param Connection $connection Connection info
+     */
+    public function __construct( Connection $connection )
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function connect() : bool
+    {
+        $this->handle = new mysqli(
+            $this->connection->host,
+            $this->connection->username,
+            $this->connection->password,
+            $this->connection->name,
+            $this->connection->port
+        );
+
+        // Connection failed?
+        return ( $this->handle->connect_errno === 0 );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function disconnect() : void
+    {
+        // TODO
+
+        return;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function query( string $sql ) : bool
+    {
+        // TODO
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResult() : array
+    {
+        // TODO
+
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResults() : array
+    {
+        // TODO
+
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLastInsertId() : int
+    {
+        // TODO
+
+        return 0;
+    }
 }
